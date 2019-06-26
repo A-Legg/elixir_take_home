d44b7fe4f145926f3eb1c4590c4a90cbe03f504a
Adam Legg <adam.legg@gmail.com>
2019-06-26 12:44:01 -0400

    Expose /spawn endpoint

--- skynet/lib/skynet/router.ex
+++ skynet/lib/skynet/router.ex
@@ -8,6 +8,16 @@ defmodule Skynet.Router do
     send_resp(conn, 200, Skynet.list_terminators())
   end
 
+  post "/spawn" do
+    case Skynet.spawn_terminator() do
+      {:ok, pid} ->
+        send_resp(conn, 200, inspect(pid))
+
+      _ ->
+        send_resp(conn, 500, "Something went wrong.")
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
--- skynet/test/skynet/router_test.exs
+++ skynet/test/skynet/router_test.exs
@@ -19,4 +19,20 @@ defmodule Skynet.RouterTest do
       Supervisor.terminate_child(Skynet.Supervisor, pid)
     end
   end
+
+  describe "/spawn" do
+    test "it can spawn a terminator" do
+      assert [] = DynamicSupervisor.which_children(Skynet.Supervisor)
+
+      conn =
+        :post
+        |> conn("/spawn")
+        |> Skynet.Router.call(@opts)
+
+      assert [{_, pid, _, _}] = DynamicSupervisor.which_children(Skynet.Supervisor)
+      assert conn.state == :sent
+      assert conn.status == 200
+      Supervisor.terminate_child(Skynet.Supervisor, pid)
+    end
+  end
 end
