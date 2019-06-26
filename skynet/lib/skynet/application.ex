04afc0fd6ecabd80032273abd5c06f4e8c652ea8
Adam Legg <adam.legg@gmail.com>
2019-06-26 13:50:05 -0400

    Add the router to otp supervision

--- skynet/lib/skynet/application.ex
+++ skynet/lib/skynet/application.ex
@@ -5,7 +5,8 @@ defmodule Skynet.Application do
 
   def start(_type, _args) do
     children = [
-      {DynamicSupervisor, strategy: :one_for_one, name: Skynet.Supervisor}
+      {DynamicSupervisor, strategy: :one_for_one, name: Skynet.Supervisor},
+      Plug.Cowboy.child_spec(scheme: :http, plug: Skynet.Router, options: [port: 4001])
     ]
 
     opts = [strategy: :one_for_one]
